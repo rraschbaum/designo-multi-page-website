0301035711954c3f6df47fed580e64f394ed3a5a
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2022-02-18 22:33:33 -0500

    add color constants

---
+++ src/components/constants.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  peach: "hsl(11deg 73% 66%)",
+  black: "hsl(270deg 3% 11%)",
+  white: "hsl(0deg 0% 100%)",
+  lightPeach: "hsl(11deg 100% 80%)",
+  darkGray: "hsl(264deg 5% 20%)",
+  lightGray: "hsl(210deg 17% 95%)",
+}
