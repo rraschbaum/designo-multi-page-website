913c24cbcb014b1e5999d637ebe586b64b4b47df
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2022-02-18 22:28:21 -0500

    update layout component import path

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Navbar from "./navbar"
-import Footer from "./Footer"
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
 
 const Layout = props => {
   return (
